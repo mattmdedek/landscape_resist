3965cbb574c467002d88798e7ed7fd599a6bc6b6
Matt Dedek <matthew.dedek@gmail.com>
2014-02-19 21:15:04 -0800

    Fixed R comments

--- script/mantel.R
+++ script/mantel.R
@@ -1,14 +1,14 @@
 install.packages("vegan")
 library(vegan)
-//read file into string
+#read file into string
 slopetxt<-readChar("Slope10.txt", file.info("Slope10.txt")$size)
-//read string into a matrix -> has to be diagonal (that's fill) and you tell it # of columns
+#read string into a matrix -> has to be diagonal (that's fill) and you tell it # of columns
 slopedmat<-data.matrix(read.table(text=slopetxt, fill=T, col.names=paste("V", 2:71)))
-//converts diagonal to symetric matrix
+#converts diagonal to symetric matrix
 slopedmat[upper.tri(slopedmat)]<-t(slopedmat)[upper.tri(slopedmat)]
 
-//do that for all your files, then mantel:
-//as.dist converts the matrix to a distance object for the test, update permutations
+#do that for all your files, then mantel:
+#as.dist converts the matrix to a distance object for the test, update permutations
 
 mantel(as.dist(eucdmat), as.dist(rousdmat), method="pearson", permutations=99)
 
