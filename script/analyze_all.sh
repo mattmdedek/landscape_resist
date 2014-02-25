b19109cf8dbaa0c2a0198ce3c228bccdbcbfd3cb
Matt Dedek <matthew.dedek@gmail.com>
2014-02-25 01:20:39 -0800

    Add skeleton for bulk analysis

---
+++ script/analyze_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python process_dir_species.py "../data/sites/CPN" "SCOC"
+python process_dir_species.py "../data/sites/JAM" "SCOC"
+python process_dir_species.py "../data/sites/JAM" "ASHY"
+python process_dir_species.py "../data/sites/LOM" "SCOC"
+python process_dir_species.py "../data/sites/LOM" "ASHY"
+python process_dir_species.py "../data/sites/SYR" "SCOC"
+python process_dir_species.py "../data/sites/TP" "SCOC"
+python process_dir_species.py "../data/sites/TP" "ASHY"
+
---
+++ script/process_dir_species.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import ls_transforms
+import glob
+import os
+import sys
+
+# take a species and a directory and run the mantel analyses
